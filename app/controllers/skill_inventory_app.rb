1f822aa22edac4a29c66dbea4a0ecb8a3ca5a4fc
PenneyGadget <dreamofgreen@gmail.com>
2015-12-01 20:49:49 -0700

    skill inventory

--- app/controllers/skill_inventory_app.rb
+++ app/controllers/skill_inventory_app.rb
@@ -1,8 +1,33 @@
+require 'models/skill_inventory'
+
 class SkillInventoryApp < Sinatra::Base
   set :root, File.join(File.dirname(__FILE__), '..')
 
+  get '/' do
+    erb :dashboard
+  end
+
+  # see all
   get '/skills' do
+    @skills = SkillInventory.all
     erb :index
+  end
+
+  # form to create new
+  get '/skills/new' do
+    erb :new
+  end
+
+  # submit new
+  post '/skills' do
+    SkillInventory.create(params[:skill])
+    redirect '/skills'
+  end
+
+  # see one
+  get 'skills/:id' do |id|
+    @skill = SkillInventory.find(id.to_i)
+    erb :show
   end
 
 end
